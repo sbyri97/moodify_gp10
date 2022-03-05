0cdbe4d5a2c52fa7298c56246096d09d6b246070
alexsmaldone <alexsmaldone@gmail.com>
2022-03-05 18:49:00 -0500

    added alex's seeder data

--- app/seeds/library.py
+++ app/seeds/library.py
@@ -20,6 +20,28 @@ def seed_library():
   sixteen = Library(artist_name="", album_name="", album_coverart_url="", song_title="", song_url="", created_at=datetime.now(), updated_at=datetime.now())
   sixteen = Library(artist_name="", album_name="", album_coverart_url="", song_title="", song_url="", created_at=datetime.now(), updated_at=datetime.now())
 
+  a66 = Library(artist_name="John Legend", album_name="Love In The Future", album_coverart_url="https://hiphop-n-more.com/wp-content/uploads/2013/07/john-legend-love-in-the-future.jpg", song_title="All of Me", song_url="https://moodify.s3.amazonaws.com/Romantic/John+Legend+-+All+of+Me.mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a67 = Library(artist_name="Marvin Gaye", album_name="Let's Get It On", album_coverart_url="https://whythebeatlesarestillrelevant.files.wordpress.com/2020/05/marvin-gaye-lets-get-it-on.jpg", song_title="Let's Get It On", song_url="https://moodify.s3.amazonaws.com/Romantic/Lets+Get+It+On+++Marvin+Gaye+Lyrics+++YouTube.mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a68 = Library(artist_name="Marvin Gaye", album_name="Let's Get It On", album_coverart_url="https://whythebeatlesarestillrelevant.files.wordpress.com/2020/05/marvin-gaye-lets-get-it-on.jpg", song_title="Please Stay (Once You Go Away)", song_url="https://moodify.s3.amazonaws.com/Romantic/Marvin+Gaye+_+Please+Stay+(Once+You+Go+Away).mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a69 = Library(artist_name="Marvin Gaye", album_name="Let's Get It On", album_coverart_url="https://whythebeatlesarestillrelevant.files.wordpress.com/2020/05/marvin-gaye-lets-get-it-on.jpg", song_title="If I Should Die Tonight", song_url="https://moodify.s3.amazonaws.com/Romantic/Marvin+Gaye+_+If+I+Should+Die+Tonight.mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a70 = Library(artist_name="Marvin Gaye", album_name="Let's Get It On", album_coverart_url="https://whythebeatlesarestillrelevant.files.wordpress.com/2020/05/marvin-gaye-lets-get-it-on.jpg", song_title="Just To Keep You Satisfied", song_url="https://moodify.s3.amazonaws.com/Romantic/Marvin+Gaye+-+Just+To+Keep+You+Satisfied.mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a71 = Library(artist_name="Whitney Houston", album_name="The Bodyguard", album_coverart_url="https://cdn.smehost.net/whitneyhoustoncom-uslegacyprod/wp-content/uploads/2018/11/04_thebodyguard_v2-1024x1024.jpg", song_title="I Will Always Love You", song_url="https://moodify.s3.amazonaws.com/Romantic/Whitney+Houston+-+I+Will+Always+Love+You+-+Lyrics.mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a72 = Library(artist_name="The Beach Boys", album_name="50 Big Ones: Greatest Hits", album_coverart_url="https://img.discogs.com/Ld28UcjCrlZKPPEgESFKBiT3S-Y=/fit-in/600x579/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-4661752-1371425577-8857.jpeg.jpg", song_title="God Only Knows", song_url="https://moodify.s3.amazonaws.com/Romantic/Beach+Boys+-+God+Only+Knows+lyrics+(Lyric+Video).mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a73 = Library(artist_name="Ed Sheeran", album_name="Divide", album_coverart_url="https://metrotoparis.com/wp-content/uploads/2017/12/C1Lg3RvUQAQqHmZ.jpg", song_title="Perfect", song_url="https://moodify.s3.amazonaws.com/Romantic/Perfect+-+Ed+Sheeran+(Lyrics).mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a74 = Library(artist_name="Thomas Rhett", album_name="Tangled Up", album_coverart_url="https://townsquare.media/site/204/files/2015/06/cover3.jpg?w=980&q=75", song_title="Die A Happy Man", song_url="https://moodify.s3.amazonaws.com/Romantic/Thomas+Rhett+-+Die+A+Happy+Man+(Lyrics+_+Lyric+Video).mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a75 = Library(artist_name="Pat Benatar", album_name="In The Heat Of The Night", album_coverart_url="https://townsquare.media/site/295/files/2014/10/Chrysalis.jpg?w=980&q=75", song_title="Heartbreaker", song_url="https://moodify.s3.amazonaws.com/Heartbreak/Pat+Benatar+-+Heartbreaker+(Lyrics).mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a76 = Library(artist_name="Adele", album_name="21", album_coverart_url="https://m.media-amazon.com/images/I/71Ln3JLWyOL._SL1500_.jpg", song_title="Someone Like You", song_url="https://moodify.s3.amazonaws.com/Heartbreak/Adele+-+Someone+Like+You+(Lyrics).mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a77 = Library(artist_name="Adele", album_name="21", album_coverart_url="https://m.media-amazon.com/images/I/71Ln3JLWyOL._SL1500_.jpg", song_title="Rolling In The Deep", song_url="https://moodify.s3.amazonaws.com/Heartbreak/Rolling+in+the+Deep+_+ADELE_+Lyrics.mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a78 = Library(artist_name="Adele", album_name="21", album_coverart_url="https://m.media-amazon.com/images/I/71Ln3JLWyOL._SL1500_.jpg", song_title="Rumour Has It", song_url="https://moodify.s3.amazonaws.com/Heartbreak/Rumor+Has+It-+Adele+Lyrics.mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a79 = Library(artist_name="Adele", album_name="21", album_coverart_url="https://m.media-amazon.com/images/I/71Ln3JLWyOL._SL1500_.jpg", song_title="Set Fire To The Rain", song_url="https://moodify.s3.amazonaws.com/Heartbreak/Adele+-+Set+Fire+To+The+Rain+LYRICS!.mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a80 = Library(artist_name="Taylor Swift", album_name="Red (Taylor's Version)", album_coverart_url="https://cdn.shopify.com/s/files/1/0011/4651/9637/products/GKg0cS-Y_dfc83adc-fffd-4f2f-bc1e-fbd16d6e9659_800x.png?v=1628098713", song_title="Red", song_url="https://moodify.s3.amazonaws.com/Heartbreak/Taylor+Swift+-+RED+(Lyric+Video).mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a81 = Library(artist_name="Taylor Swift", album_name="Red (Taylor's Version)", album_coverart_url="https://cdn.shopify.com/s/files/1/0011/4651/9637/products/GKg0cS-Y_dfc83adc-fffd-4f2f-bc1e-fbd16d6e9659_800x.png?v=1628098713", song_title="I Knew You Were Trouble", song_url="https://moodify.s3.amazonaws.com/Heartbreak/Taylor+Swift+-+I+Knew+You+Were+Trouble+(Lyrics).mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a82 = Library(artist_name="Taylor Swift", album_name="Red (Taylor's Version)", album_coverart_url="https://cdn.shopify.com/s/files/1/0011/4651/9637/products/GKg0cS-Y_dfc83adc-fffd-4f2f-bc1e-fbd16d6e9659_800x.png?v=1628098713", song_title="All Too Well", song_url="https://moodify.s3.amazonaws.com/Heartbreak/All+Too+Well+_+Taylor+Swift+(Lyrics).mp3", created_at=datetime.now(), updated_at=datetime.now())
+  a83 = Library(artist_name="Taylor Swift", album_name="Red (Taylor's Version)", album_coverart_url="https://cdn.shopify.com/s/files/1/0011/4651/9637/products/GKg0cS-Y_dfc83adc-fffd-4f2f-bc1e-fbd16d6e9659_800x.png?v=1628098713", song_title="We Are Never Ever Getting Back Together", song_url="https://moodify.s3.amazonaws.com/Heartbreak/Taylor+Swift+-+We+Are+Never+Ever+Getting+Back+Together+(Lyrics).mp3", created_at=datetime.now(), updated_at=datetime.now())
+
+
+
+
 # add more as needed,
 
   # Joan one through twentyseven
@@ -35,6 +57,25 @@ def seed_library():
   # after last song instance
   db.session.add(one)
   db.session.add(two)
+
+  db.session.add(a66)
+  db.session.add(a67)
+  db.session.add(a68)
+  db.session.add(a69)
+  db.session.add(a70)
+  db.session.add(a71)
+  db.session.add(a72)
+  db.session.add(a73)
+  db.session.add(a74)
+  db.session.add(a75)
+  db.session.add(a76)
+  db.session.add(a77)
+  db.session.add(a78)
+  db.session.add(a79)
+  db.session.add(a80)
+  db.session.add(a81)
+  db.session.add(a82)
+  db.session.add(a83)
   # continue adding for all song instances once data is consolidated
 
   db.session.commit()
--- app/seeds/playlists.py
+++ app/seeds/playlists.py
@@ -19,6 +19,13 @@ def seed_playlists():
   two = Playlist(user_id=1, name="Party Playlist", mood_id=1, created_at=datetime.now(), updated_at=datetime.now())
   three = Playlist(user_id=1, name="Celebration!", mood_id=1, created_at=datetime.now(), updated_at=datetime.now())
 
+  a22 = Playlist(user_id=1, name="Passion", mood_id=3, created_at=datetime.now(), updated_at=datetime.now())
+  a23 = Playlist(user_id=1, name="Infatuation", mood_id=3, created_at=datetime.now(), updated_at=datetime.now())
+  a24 = Playlist(user_id=1, name="Yearning", mood_id=3, created_at=datetime.now(), updated_at=datetime.now())
+  a25 = Playlist(user_id=1, name="Heartache", mood_id=8, created_at=datetime.now(), updated_at=datetime.now())
+  a26 = Playlist(user_id=1, name="It's Over", mood_id=8, created_at=datetime.now(), updated_at=datetime.now())
+  a27 = Playlist(user_id=1, name="No Shot for Second Chances", mood_id=8, created_at=datetime.now(), updated_at=datetime.now())
+
 # Joan playlists one through nine, Katerina playlists ten through fifteen, Sai playlists sixteen through twentyone, Alex playlists twentytwo through twentyseven
 
 
@@ -26,6 +33,13 @@ def seed_playlists():
   # after last playlist instance
   db.session.add(one)
   db.session.add(two)
+
+  db.session.add(a22)
+  db.session.add(a23)
+  db.session.add(a24)
+  db.session.add(a25)
+  db.session.add(a26)
+  db.session.add(a27)
   # continue adding for all playlist instances once data is consolidated
 
   db.session.commit()
