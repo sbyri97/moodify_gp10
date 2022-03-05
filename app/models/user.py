e2e9d075dcaaf807e59348432314b115b52d5df5
alexsmaldone <alexsmaldone@gmail.com>
2022-03-05 02:02:10 -0500

    changes to user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,15 +1,24 @@
 from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
     hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    playlists = db.relationship("Playlist", back_populates="user")
+    following = db.relationship("Follows", back_populates="user_following")
+    followed = db.relationship("Follows", back_populates="user_followed")
 
     @property
     def password(self):
