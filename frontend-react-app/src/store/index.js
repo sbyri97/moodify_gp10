e81d98c07e033ed65eb032277776415d4696cf94
alexsmaldone <alexsmaldone@gmail.com>
2022-03-07 18:36:22 -0500

    created playlist files

--- frontend-react-app/src/store/index.js
+++ frontend-react-app/src/store/index.js
@@ -2,12 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import session from "./session";
 import libraryReducer from "./library";
-import searchSongReducer from './searchsong';
+import searchSongReducer from "./searchsong";
 
 const rootReducer = combineReducers({
   session,
   libraryReducer,
-  searchSongReducer
+  // searchSongReducer
 });
 
 let enhancer;
