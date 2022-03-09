2445b5d127395d8ab9381d97fdf06e4599cda8d5
Code-rina <katerina.kreibich@gmail.com>
2022-03-09 08:09:03 -0800

    demo user working

---
+++ frontend-react-app/src/components/NavBar/demouser.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import * as sessionActions from '../../store/session';
+
+const DemoButton = () => {
+  const user = useSelector(state => state.session.user);
+  const dispatch = useDispatch()
+  const handleDemo = async (e) => {
+    e.preventDefault();
+    const email = 'admin@moodify.com';
+    const password = 'password'
+    await dispatch(sessionActions.login(email, password));
+    
+    };
+    
+    if (user) {
+        return <Redirect to='/' />;
+    }
+    
+        return <button id='demo-btn' onClick={handleDemo}>Demo</button>;
+    }
+    
+    
+export default DemoButton;
