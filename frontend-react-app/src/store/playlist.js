0e131181d3e490a810ecb671df4814530a46829d
alexsmaldone <alexsmaldone@gmail.com>
2022-03-09 18:11:19 -0500

    wrote reducer for getMoodPlaylists

--- frontend-react-app/src/store/playlist.js
+++ frontend-react-app/src/store/playlist.js
@@ -1,6 +1,6 @@
 const LOAD_PLAYLIST = "playlists/loadPlaylist";
 const LOAD_PLAYLISTS = "playlists/loadPlaylists";
-const LOAD_MOOD_PLAYLISTS = "playlists/loadMoodPlaylists"
+const LOAD_MOOD_PLAYLISTS = "playlists/loadMoodPlaylists";
 
 // ---------------------------------------
 
@@ -22,11 +22,10 @@ export const loadMoodPlaylists = (playlists) => {
   return {
     type: LOAD_MOOD_PLAYLISTS,
     playlists,
-  }
-}
+  };
+};
 
 // ---------------------------------------
-
 
 export const getArtistSongs = (artistName) => async (dispatch) => {
   const response = await fetch(`/api/artists/${artistName}`);
@@ -48,48 +47,59 @@ export const getAlbumSongs = (albumName) => async (dispatch) => {
   return response;
 };
 
-export const getPlaylist = (playlistId) => async(dispatch) => {
-    const response = await fetch(`/api/playlists/${playlistId}`)
+export const getPlaylist = (playlistId) => async (dispatch) => {
+  const response = await fetch(`/api/playlists/${playlistId}`);
 
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(loadPlaylist(data));
+  }
+  return response;
+};
+
+export const getPlaylists = () => async (dispatch) => {
+  const response = await fetch("/api/playlists");
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(loadPlaylists(data.playlists));
+  }
+  return response;
+};
+
+export const getMoodPlaylists = (id) => async (dispatch) => {
+  const response = await fetch(`/api/moods/${id}`);
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(loadMoodPlaylists(data.playlists));
+  }
+  return response;
+};
+
+export const createPlaylist =
+  ({ name, mood_id, user_id }) =>
+  async (dispatch) => {
+    const response = await fetch("/api/playlists/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name,
+        mood_id,
+        user_id,
+      }),
+    });
+    console.log(response);
     if (response.ok) {
-        const data = await response.json();
-        dispatch(loadPlaylist(data))
+      const data = await response.json();
+      dispatch(loadPlaylist(data.playlists));
+      return data.playlists;
     }
-    return response;
-}
-
-export const getPlaylists = () => async(dispatch) => {
-    const response = await fetch('/api/playlists')
-
-    if (response.ok) {
-        const data = await response.json();
-        dispatch(loadPlaylists(data.playlists))
-    }
-    return response;
-}
-
-export const createPlaylist = ({name, mood_id, user_id}) => async(dispatch) => {
-    const response = await fetch('/api/playlists/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            name,
-            mood_id,
-            user_id
-        })
-    })
-    console.log(response)
-    if(response.ok) {
-        const data = await response.json();
-        dispatch(loadPlaylist(data.playlists))
-        return data.playlists
-    }
-}
+  };
 
 // ---------------------------------------
-const initialState = { playlists: {} };
+const initialState = { playlists: {}, mood: {} };
 const playlistReducer = (state = initialState, action) => {
-
   switch (action.type) {
     case LOAD_PLAYLIST: {
       const playlists = {
@@ -98,13 +108,20 @@ const playlistReducer = (state = initialState, action) => {
       };
       return { ...state, playlists };
     }
-     case LOAD_PLAYLISTS: {
-       const playlists = {}
-       action.playlists.forEach(playlist => {playlists[playlist.name] = playlist})
-       return {...state, playlists}
-        }
-      default:
-        return state
+    case LOAD_PLAYLISTS: {
+      const playlists = {};
+      action.playlists.forEach((playlist) => {
+        playlists[playlist.name] = playlist;
+      });
+      return { ...state, playlists };
+    }
+    case LOAD_MOOD_PLAYLISTS: {
+      const newState = { ...state };
+      newState.mood = action.playlists;
+      return newState;
+    }
+    default:
+      return state;
   }
 };
 
