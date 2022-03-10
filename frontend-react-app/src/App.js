76ea17d165e817054f43bf87bdf2639448973fcf
Sai Byri <saikiran169@gmail.com>
2022-03-10 18:51:35 -0500

    middle of nav stuff

--- frontend-react-app/src/App.js
+++ frontend-react-app/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import LoginForm from "./components/auth/LoginFormModal/LoginForm";
 import SignUpForm from "./components/auth/SignUpFormModal/SignUpForm";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import UsersList from "./components/UsersList";
 import User from "./components/User";
