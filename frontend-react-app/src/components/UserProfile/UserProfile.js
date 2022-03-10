63c03dc52eac8af632dee4b63cfe5e4873c4e8e6
alexsmaldone <alexsmaldone@gmail.com>
2022-03-10 00:31:03 -0500

    begginings of user profile page

--- frontend-react-app/src/components/UserProfile/UserProfile.js
+++ frontend-react-app/src/components/UserProfile/UserProfile.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
+import "./UserProfile.css";
 
 function UserProfile() {
+  const userParam = useParams();
+  const userId = userParam.userId;
+  console.log("USER====", userId);
   return (
-    <div>
-      <div>Hello World</div>
+    <div className="user-profile-page-container">
+      <div className="user-profile-top-section">
+        <div>
+          <FaUserCircle className="user-circle" />
+        </div>
+        <div className="user-profile-info">
+          <h1 className="user-profile-username">UserName</h1>
+          <button className="user-profile-follow-button">Follow</button>
+        </div>
+      </div>
+      <div className="user-profile-bottom-section">Hello World</div>
     </div>
   );
 }
