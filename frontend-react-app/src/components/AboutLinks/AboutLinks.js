c2c2ff72cf650b1fd1067f724b2a83c413306252
alexsmaldone <alexsmaldone@gmail.com>
2022-03-12 01:43:42 -0500

    getting started on adding aboutlinks page

--- frontend-react-app/src/components/AboutLinks/AboutLinks.js
+++ frontend-react-app/src/components/AboutLinks/AboutLinks.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
+import "./AboutLinks.css";
 
 function AboutLinks() {
   return (
     <div className="about-page-container">
-      <div className="about-top-container">somethin something</div>
+      <div className="about-top-container">
+        <h1>Meet the Moodify Team</h1>
+      </div>
       <div className="about-bottom-container">somethin something</div>
     </div>
   );
