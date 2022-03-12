2f4dd87b6d63a8caf70b1123c7d9e772c90f4a9a
Sai Byri <saikiran169@gmail.com>
2022-03-11 20:29:00 -0500

    routes to unauth from playlist modal on home

--- frontend-react-app/src/components/Playlist/index.js
+++ frontend-react-app/src/components/Playlist/index.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { Modal } from '../../context/Modal';
 import NewPlaylistForm from './NewPlaylist';
 import './index.css'
@@ -7,13 +9,17 @@ import './index.css'
 function PlaylistFormModal() {
     const [showModal, setShowModal] = useState(false)
     const sessionUser = useSelector((state) => state.session.user)
+    const history = useHistory()
 
-    
+    const modalunauth = (e) => {
+        e.stopPropagation()
+        history.push('/unauthenticatedUser')
+    }
 
     return (
         <div className='playlistFormModal'>
             {(!sessionUser) ?
-            <button className='createPlaylistButton' onClick={() => setShowModal(true)}>
+            <button className='createPlaylistButton' onClick={modalunauth}>
                 <i className='fa-solid fa-square-plus'></i>
                 <div className='create-new-playlist-icon-text'>Create New Playlist</div>
             </button>
