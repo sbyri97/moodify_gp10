b5e053d5e8d9052d9b3cd9831bb0ca13a46e3f30
Sai Byri <saikiran169@gmail.com>
2022-03-12 01:14:18 -0500

    demo routes to home

--- frontend-react-app/src/components/NavBar/demouser.js
+++ frontend-react-app/src/components/NavBar/demouser.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import * as sessionActions from '../../store/session';
 import './navbar.css'
 
 const DemoButton = () => {
-  const user = useSelector(state => state.session.user);
+  const user = useSelector(state => state.session?.user);
   const dispatch = useDispatch()
+  const history = useHistory()
+
   const handleDemo = async (e) => {
     e.preventDefault();
     const email = 'bobbie@aa.io';
     const password = 'password'
     await dispatch(sessionActions.login(email, password));
-
-    };
+    history.push('/')
+};
 
     if (user) {
         return <Redirect to='/' />;
